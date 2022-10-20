40d9717068d53f0cb01b938e252dc4e82c32a414
Queen uche <queenuche33@gmail.com>
2022-10-20 14:20:36 -0700

    Task zero

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 -a function that prints 10 times the alphabet
+ * print_alphabet_x10 - a function that prints 10 times the alphabet
  *
  * Return: x10 a-z
  */
-
 void print_alphabet_x10(void)
 {
-	int n,co;
+	int n, co;
+
 	co = 0;
 
-	while (c < 10)
+	while (co < 10)
 	{
-		for (n ='a'; n <= 'z'; n++)
+		for (n = 'a'; n <= 'z'; n++)
 		{
 			_putchar(n);
 		}
